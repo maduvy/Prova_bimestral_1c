6822b4a3a75b914fd708c5ba9ce4b70527245a47
maduvy <lourencomariaeduarda73@gmail.com>
2023-09-25 09:03:03 -0300

    prova
    tentamos mais uma vez

--- prova/prova.py
+++ prova/prova.py
@@ -1,9 +1,12 @@
 nome=input('qual é o seu nome? ')
 #aqui a pessoa dirá o seu nome
-n=int(input('qual a sua idade? '))
+n1=int(input('qual a sua idade? '))
 #aqui a pessoa dirá a sua idade
-n=float(input('qual a sua altura? ' ))
+n2=float(input('qual a sua altura? ' ))
 #aqui dirá a altura dela 
-n=float(input('qual é o seu peso? '))
+n3=float(input('qual é o seu peso? '))
 #e por fim aqui ela dirá o seu peso
-
+print('olá me chamo' ,nome)
+print('minha idade é' ,n1 )
+print('minha altura é' ,n2)
+print('e meu peso é' ,n3)
