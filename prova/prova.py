d7d3dbff5b9d7da11acbcfdc03462be41a75ac33
maduvy <lourencomariaeduarda73@gmail.com>
2023-09-25 08:54:04 -0300

    prova bimestral
    seja o que sergio quiser

---
+++ prova/prova.py
@@ -0,0 +1,9 @@
+nome=input('qual é o seu nome? ')
+#aqui a pessoa dirá o seu nome
+n=int(input('qual a sua idade? '))
+#aqui a pessoa dirá a sua idade
+n=float(input('qual a sua altura? ' ))
+#aqui dirá a altura dela 
+n=float(input('qual é o seu peso? '))
+#e por fim aqui ela dirá o seu peso
+
