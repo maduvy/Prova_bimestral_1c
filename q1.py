027edae5e7f6e60189bf4e1097d5cebc85b95ff3
maduvy <143403284+maduvy@users.noreply.github.com>
2023-10-10 11:14:54 -0300

    v1

---
+++ q1.py
@@ -0,0 +1,8 @@
+altura=float(input('digite a altura'))
+largura=float(input('digite a largura'))
+area=altura*largura
+print('a area de sua parede é: ',area,'m²')
+tinta=area/2
+print('a tinta que voce ira usar pra pintar é:',tinta,'litros')
+print('boa sorte na pintura:)')
+      
