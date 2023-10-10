d5974dbdbaa03a80ceb31f06d97f151e5f1ef242
maduvy <143403284+maduvy@users.noreply.github.com>
2023-10-10 11:46:05 -0300

    continuaçao_da_lista01

---
+++ lista01.py/atv3.py
@@ -0,0 +1,4 @@
+numero=int(input('digite um numero'))
+numero2=int(input('digite outro numero'))
+r=numero+numero2
+print('a soma dos dois numeros é:',r)
---
+++ lista01.py/atv4.py
@@ -0,0 +1,6 @@
+nota1=float(input('digite sua primeira nota'))
+nota2=float(input('digite sua segunda nota'))
+nota3=float(input('digite sua terceira nota'))
+nota4=float(input('digite sua quarta nota'))
+r=(nota1+nota2+nota3+nota4)/4
+print('sua média e:',r)
